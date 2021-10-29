4c9d44aeb1fef73abb314b733c52f6d28706fada
Desmond Madden <desmond.madden@hotmail.co.uk>
2021-10-29 23:04:59 +0100

    Adding more code

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-import mysql.connector
+from Setup import config
+import mysql.connector as connector
 
 def main():
   print("Hello World!")
+  conn = connector.connect(
+    user = config.username,
+    password = config.password,
+    database = config.db)
+
+  cur = conn.cursor()
+
+  cur.execute("SELECT * FROM author")
+  row = cur.fetchone()
+  print(row[1])
+
+
+
+  
 
 if __name__ == "__main__":
     main()
